8da0b03c44ff3b340453ee07a8d0e88ad293cb84
Missbahul <hassanmissbahul@gmail.com>
2025-07-27 17:36:26 +0530

    git config

--- 01.py
+++ 01.py
@@ -1,5 +1,12 @@
 print("Hello World !!!")
 
 
+
+
+
 # py filename.py
 # py 01.py
+
+
+# git config --global user.name ".............urname"
+# git config --global user.email ".................@gmail.com"
